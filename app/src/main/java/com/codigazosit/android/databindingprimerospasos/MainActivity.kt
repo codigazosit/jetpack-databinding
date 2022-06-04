30ee8d27b9ce04300aa826b09924aad3c1918034
Codigazos IT <codigazosit@gmail.com>
2022-06-04 16:16:23 -0500

    Ejemplo Jetpack: DataBinding - Primeros pasos.

---
+++ app/src/main/java/com/codigazosit/android/databindingprimerospasos/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.codigazosit.android.databindingprimerospasos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.codigazosit.android.databindingprimerospasos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.greeting = "Hola DataBinding"
+    }
+}
+
